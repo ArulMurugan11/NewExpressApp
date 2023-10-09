e75a7dddf43eb3557c59b790b6b3b09c7c412624
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2023-10-09 16:20:24 +0530

    Initial commit

---
+++ src/models/login.js
@@ -0,0 +1,25 @@
+module.exports = function loginSchema(sequelize, DataType) {
+    const Login = sequelize.define('Login', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        userName: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataType.STRING,
+            allowNull: false,
+        }
+    }, {
+        timestamps: true,
+        underscored: true,
+        tableName: 'login',
+    });
+    Login.associate = function mapRelation() {
+    };
+    return Login;
+};
---
+++ src/models/task.js
@@ -0,0 +1,25 @@
+module.exports = function taskSchema(sequelize, DataType) {
+    const Task = sequelize.define('Task', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        title: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataType.TEXT,
+            allowNull: true,
+        },
+    }, {
+        timestamps: true,
+        underscored: true,
+        tableName: 'task',
+    });
+    Task.associate = function mapRelation() {
+    };
+    return Task;
+};
